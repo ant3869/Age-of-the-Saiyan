add485dd661c4fd82737067724a722cd9d780e65
leemarov <leemarov@gmail.com>
2023-06-10 12:44:33 -0500

    remove minimum_game

--- addons/minimum_game/src/Entity.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-
-func player_entered(_body):
-	pass # Replace with function body.
--- addons/minimum_game/src/Trap.gd
+++
@@ -1,6 +0,0 @@
-extends "res://src/Entity.gd"
-
-func player_entered(body):
-	if body.has_method("die"):
-		$TrapSound.play()
-		body.die(null, "")
